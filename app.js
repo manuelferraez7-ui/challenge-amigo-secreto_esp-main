96d08cfa92320e0e9fde46e67bea3dca749ba3ca
Manuel Alejandro Ferraez Argüelles <manuelferraez7@gmail.com>
2025-08-12 13:51:07 -0600

    actualiza el app.js

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-console.log("Ejecutando el sistema")
+alert ("BIENVENIDO AL RETO");
+//Variables
+let nombreSecreto = Manuel;
+let nombreUsuario = prompt("Me indicas un nombre por favor:");
+
+console.log(nombreUsuario);
+
+if (nombreUsuario == nombreSecreto) {
+    alert('Acertaste el nombre');
+}
